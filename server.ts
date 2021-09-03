bab25ed1d9eac462550ede7e05880c6bbfeac0e4
Eagle Ikechukwu <mightyeaglecorp@gmail.com>
2021-09-03 10:41:27 +0100

    made change to port

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { app } from './app';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { app } from "./app";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
-let DATABASE = process.env.DATABASE || '';
-let DATABASE_LOCAL = process.env.DATABASE_LOCAL || '';
+let DATABASE = process.env.DATABASE || "";
+let DATABASE_LOCAL = process.env.DATABASE_LOCAL || "";
 
 mongoose
   .connect(DATABASE, {
@@ -16,12 +16,13 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('DB Connected successfully');
+    console.log("DB Connected successfully");
   })
   .catch((e) => {
-    console.log('DB could not connect at this time');
+    console.log("DB could not connect at this time");
     process.exit(1);
   });
-app.listen(process.env.PORT, () => {
-  console.log(`${process.env.PORT} is listening`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`${port} is listening`);
 });
