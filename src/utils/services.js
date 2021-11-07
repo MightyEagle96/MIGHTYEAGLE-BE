3899ddff0a0ea76784f504bbdca70c9bc76c78ff
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2021-11-07 23:25:37 +0100

    made some changes

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.originUrl = void 0;
-exports.originUrl = process.env.NODE_ENV === 'development'
-    ? 'https://hidden-fjord-42920.herokuapp.com'
-    : 'http://localhost:3000';
+// export const originUrl =
+//   process.env.NODE_ENV === 'development'
+//     ? 'https://hidden-fjord-42920.herokuapp.com'
+//     : 'http://localhost:3000';
+exports.originUrl = 'https://me-school.herokuapp.com';
+//export const originUrl = 'http://localhost:3000'
--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,4 +1,6 @@
-export const originUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'https://hidden-fjord-42920.herokuapp.com'
-    : 'http://localhost:3000';
+// export const originUrl =
+//   process.env.NODE_ENV === 'development'
+//     ? 'https://hidden-fjord-42920.herokuapp.com'
+//     : 'http://localhost:3000';
+export const originUrl = 'https://me-school.herokuapp.com'
+//export const originUrl = 'http://localhost:3000'
