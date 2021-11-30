6e78ba9c66de547d99e8cc7cddc42cfbc1539bf6
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2021-12-01 00:09:36 +0100

    made some changes

--- trial.js
+++ trial.js
@@ -1,1 +1,11 @@
- 
+let examTime = 10000;
+
+function getCountDown() {
+  const timer = setInterval(() => {
+    console.log((examTime -= 1000));
+
+    if (examTime === 0) clearInterval(timer);
+  }, 1000);
+}
+
+getCountDown();
